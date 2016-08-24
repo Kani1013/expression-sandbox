458ccbb75dea395b3aefa741946ea7da8b9336c0
Joshua Wise <jwise@hearst.com>
2016-08-24 14:49:49 -0400

    added tests

--- test/11.security.js
+++ test/11.security.js
@@ -3,15 +3,6 @@ var compiler = require('../.')
 global.foo = 123
 
 describe('An attacker', function () {
-	it('cannot use string manipulation to execute unsandboxed code', function () {
-		expect(compiler('}).call(this)} 123; if (false) {(function () {')({})).to.equal(undefined)
-	})
-	it('cannot use functions to reveal the global object', function () {
-		expect(compiler('}).call(this), function () {return this.foo}(); (function () {')({})).to.not.equal(123)
-	})
-	it('cannot use eval to reveal the global object', function () {
-		expect(compiler('2..constructor.constructor("return this.foo")()')).to.not.equal(123)
-	})
 	it('cannot receive the global object directly', function () {
 		expect(function () {compiler('global.foo')({})}).to.throw(TypeError)
 		expect(function () {compiler('foo')({foo: global})}).to.throw(TypeError)
@@ -19,4 +10,20 @@ describe('An attacker', function () {
 		expect(function () {compiler('this.foo')({foo: global})}).to.throw(TypeError)
 		expect(function () {compiler('foo()')({foo: function () {return global}})}).to.throw(TypeError)
 	})
+	it('cannot use functions to reveal the global object', function () {
+		expect(compiler('}).call(this), function () {return this.foo}(); (function () {')({})).to.not.equal(123)
+	})
+	it('cannot use eval to reveal the global object', function () {
+		expect(compiler('2..constructor.constructor("return this.foo")()')({})).to.not.equal(123)
+		expect(compiler('eval("this.foo")')({eval})).to.not.equal(123)
+	})
+	it('cannot use string manipulation to execute unsandboxed code', function () {
+		expect(compiler('}).call(this)} 123; if (false) {(function () {')({})).to.equal(undefined)
+	})
+	it('cannot manipulation the sandbox context object')
+	it('cannot manipulation objects that are passed into the sandbox')
+	it('cannot manipulation accessible prototypes')
+	it('cannot manipulation objects that are available on accessible prototypes')
+	it('cannot access outside scope')
+	it('cannot access properties that start with "_')
 })
