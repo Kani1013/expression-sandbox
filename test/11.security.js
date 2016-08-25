771536e80d0383ab0ff37a9b28ad96e920941e56
Joshua Wise <jwise@hearst.com>
2016-08-25 11:37:25 -0400

    finished security tests

--- test/11.security.js
+++ test/11.security.js
@@ -34,10 +34,93 @@ describe('An attacker', function () {
 			compiler('}).call(this)} 123; if (false) {(function () {')({})
 		}).to.throw(SyntaxError)
 	})
-	it('cannot manipulate the sandbox context object')
-	it('cannot manipulate objects that are passed into the sandbox')
-	it('cannot manipulate accessible prototypes')
-	it('cannot manipulate objects that are available on accessible prototypes')
-	it('cannot access outside scope')
-	it('cannot access properties that start with "_')
+	it('cannot manipulate the sandbox context object', function () {
+		expect(function () {
+			compiler('this.foo = 2')({})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.setPrototypeOf(this, {})')({Object})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.preventExtensions(this)')({Object})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.defineProperty(this, "foo", {value: 2})')({Object})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('delete this.foo')({Object, foo: 2})
+		}).to.throw(TypeError)
+	})
+	it('cannot manipulate objects that are passed into the sandbox', function () {
+		var obj = {foo: 2, fn: function () {return {}}}
+		expect(function () {
+			compiler('obj.bar = 2')({obj})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.setPrototypeOf(obj, {})')({Object, obj})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.preventExtensions(obj)')({Object, obj})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.defineProperty(obj, "bar", {value: 2})')({Object, obj})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('delete obj.foo')({Object, obj})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('obj.fn().foo = 2')({Object, obj})
+		}).to.throw(TypeError)
+	})
+	it('cannot manipulate accessible prototypes', function () {
+		expect(function () {
+			compiler('2..constructor.prototype.foo = 2')({})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.setPrototypeOf(2..constructor.prototype, {})')({Object})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.preventExtensions(2..constructor.prototype)')({Object})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.defineProperty(2..constructor.prototype, "foo", {value: 2})')({Object})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('delete 2..constructor.prototype.toFixed')({Object})
+		}).to.throw(TypeError)
+	})
+	it('cannot manipulate objects that are available on accessible prototypes', function () {
+		expect(function () {
+			compiler('2..constructor.prototype.toFixed.foo = 2')({})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.setPrototypeOf(2..constructor.prototype.toFixed, {})')({Object})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.preventExtensions(2..constructor.prototype.toFixed)')({Object})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Object.defineProperty(2..constructor.prototype.toFixed, "foo", {value: 2})')({Object})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('delete 2..constructor.prototype.toFixed.foo')({Object})
+		}).to.throw(TypeError)
+	})
+	it('cannot access outside scope', function () {
+		expect(function () {
+			var foo = {bar: 2}
+			compiler('foo.bar')({})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Math.round(2)')({})
+		}).to.throw(TypeError)
+		expect(function () {
+			compiler('Math.round(2)')({Math})
+		}).to.not.throw()
+	})
+	it('cannot access properties that start with "_', function () {
+		expect(compiler('foo.bar')({foo: {bar: 2}})).to.equal(2)
+		expect(compiler('foo._bar')({foo: {_bar: 2}})).to.be.undefined
+		expect(compiler('_foo')({_foo: 2})).to.be.undefined
+	})
 })
